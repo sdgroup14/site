7e96eae9aa763f976825177db8b777189c5cb5f6
Daniel Soloviov <sdgroup14@gmail.com>
2017-08-21 14:02:04 +0300

    clear finish

--- js/phones.js
+++ js/phones.js
@@ -1,68 +1,66 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('test.phones', ['ui.router'])
-        .config(['$stateProvider', '$urlRouterProvider', config])
-        .controller('PhonesCtrl', PhonesCtrl);
+  angular
+    .module('test.phones', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', config])
+    .controller('PhonesCtrl', PhonesCtrl);
 
-    PhonesCtrl.$inject = ['$scope', '$rootScope', '$http'];
+  PhonesCtrl.$inject = ['$scope', '$rootScope', '$http'];
 
-    function PhonesCtrl($scope, $rootScope, $http) {
-        $rootScope.pageTitle = 'Test - Phones';
-        $scope.items = [{
-                name: 'Nokia',
-                url: 'nokia'
-            },
-            {
-                name: 'HTC',
-                url: 'htc'
-            },
-            {
-                name: 'Apple',
-                url: 'apple'
-            }
-        ];
+  function PhonesCtrl($scope, $rootScope, $http) {
+    $rootScope.pageTitle = 'Test - Phones';
+    $scope.items = [{
+        name: 'Nokia',
+        url: 'nokia'
+      },
+      {
+        name: 'HTC',
+        url: 'htc'
+      },
+      {
+        name: 'Apple',
+        url: 'apple'
+      }
+    ];
 
-        console.log($scope.items[0].name);
-        
-        $http({
-            method: 'get',
-            url: '../json/phones.json'
-        }).then(function(response) {
-            var nokia = response.data.nokia;
-            var htc = response.data.htc;
-            var apple = response.data.apple;
-            $scope.nokiaData = nokia;
-            $scope.htcData = htc;
-            $scope.appleData = apple;
-        }, function(error) {
-            console.log(error);
-        });
-    };
+    $http({
+      method: 'get',
+      url: '../json/phones.json'
+    }).then(function(response) {
+      var nokia = response.data.nokia;
+      var htc = response.data.htc;
+      var apple = response.data.apple;
+      $scope.nokiaData = nokia;
+      $scope.htcData = htc;
+      $scope.appleData = apple;
+    }, function(error) {
+      console.log(error);
+    });
+  };
 
 
-    function config($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('root.phones', {
-                url: '/phones',
-                templateUrl: '../views/pages/phones/phones.html',
-                controller: PhonesCtrl
-            })
-            .state('nokia', {
-                url: '/nokia',
-                parent: 'root.phones',
-                templateUrl: '../views/pages/phones/phones.nokia.html'
-            })
-            .state('htc', {
-                url: '/htc',
-                parent: 'root.phones',
-                templateUrl: '../views/pages/phones/phones.htc.html'
-            })
-            .state('apple', {
-                url: '/apple',
-                parent: 'root.phones',
-                templateUrl: '../views/pages/phones/phones.apple.html'
-            })
-    };
+  function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('root.phones', {
+        url: '/phones',
+        templateUrl: '../views/pages/phones/phones.html',
+        controller: PhonesCtrl
+      })
+      .state('nokia', {
+        url: '/nokia',
+        parent: 'root.phones',
+        templateUrl: '../views/pages/phones/phones.nokia.html'
+      })
+      .state('htc', {
+        url: '/htc',
+        parent: 'root.phones',
+        templateUrl: '../views/pages/phones/phones.htc.html'
+      })
+      .state('apple', {
+        url: '/apple',
+        parent: 'root.phones',
+        templateUrl: '../views/pages/phones/phones.apple.html'
+      })
+  };
 })();
